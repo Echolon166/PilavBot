04dba6f736f8049b8aa6c453ac0c093e8d13ee19
Echolon166 <erdemdeger11@hotmail.com>
2021-01-06 21:53:45 +0300

    Add basics of root_setup

--- pilavbot/cogs/channel_cog.py
+++ pilavbot/cogs/channel_cog.py
@@ -47,3 +47,45 @@ class ChannelCommands(commands.Cog):
         traceback.print_exception(
             type(error), error, error.__traceback__, file=sys.stderr
         )
+
+
+    @commands.command(
+        name="root_setup", 
+        help="Setup a new root game"
+    )
+    @commands.guild_only()
+    async def root_setup(self, ctx):
+        #setup_message = await ctx.send(f"Setting up a new root game!\nThe author should add {START_EMOJI_STR} to start.\n\nPlease add {JOIN_EMOJI_STR} as a reaction if you want to join.")
+        setup_message = await pretty_print(
+            ctx,
+            f"The author should add {START_EMOJI_STR} to start.\n\nPlease add {JOIN_EMOJI_STR} as a reaction if you want to join.",
+            title="Setting up a new root game!",
+            color=SUCCESS_COLOR,
+        )
+        await setup_message.add_reaction(emoji=JOIN_EMOJI_STR)
+        await setup_message.add_reaction(emoji=START_EMOJI_STR)
+
+        while(True):
+            reaction = await self.bot.wait_for('reaction_add', check=lambda reaction, author: author == ctx.author)
+
+            if(str(reaction[0].emoji) == START_EMOJI_STR and reaction[0].message.id == setup_message.id):
+                break
+
+        msg = await ctx.fetch_message(setup_message.id)
+
+        users = []
+        for reaction in msg.reactions:
+            if str(reaction) == JOIN_EMOJI_STR:
+                async for user in reaction.users():
+                    if user.bot != True:
+                        users.append(user)
+                break
+
+        await setup_message.delete()
+        
+        await pretty_print(
+            ctx,
+            f"{', '.join(user.name for user in users)}",
+            title="Players",
+            color=SUCCESS_COLOR,
+        )
